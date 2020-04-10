3b9e86a4b67b9952f0c9bceceae36afe5ee4e266
Florian Guitton <florian@secretarium.com>
2020-04-10 13:34:14 +0100

    Adding Storybook

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 
 import { AppRegistry } from 'react-native';
+// import StorybookUI from '../storybook';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// if (__DEV__)
+//     AppRegistry.registerComponent(appName, () => StorybookUI);
+// else
 AppRegistry.registerComponent(appName, () => App);
