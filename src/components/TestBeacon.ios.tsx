cee2bdd6709e605786af823b48a8a8d1a276f006
bfoing <bertrand@secretarium.org>
2020-04-13 23:39:09 +0100

    Use on device ready event

--- src/components/TestBeacon.ios.tsx
+++ src/components/TestBeacon.ios.tsx
@@ -15,22 +15,24 @@ export const TestBeacon: React.FC = () => {
         // });
 
         BLEAdvertiser.setCompanyId(0xFFFF);
-        BLEAdvertiser.broadcast('00420000-0000-0000-0000-000000000000', [12, 23, 56]) // The UUID you would like to advertise and additional manufacturer data. 
-            .then(success => {
-                setSuccessBroad(success);
-                console.log('iOS, Broadcasting Sucessful', success);
-            })
-            .catch(error => console.log('iOS, Broadcasting Error', error));
-        BLEAdvertiser.scan([12, 23, 56], {})
-            .then(success => {
-                setSuccessScan(success);
-                eventEmitter.addListener('onDeviceFound', (event) => {
-                    console.log(event) // "device data"
-                    setDevices([...devices, event]);
-                });
-                console.log('iOS, Scan Sucessful', success);
-            })
-            .catch(error => console.log('iOS, Scan Error', error));
+        eventEmitter.addListener('onDeviceReady', () => {
+            BLEAdvertiser.broadcast('00420000-0000-0000-0000-000000000000', [12, 23, 56]) // The UUID you would like to advertise and additional manufacturer data. 
+                .then(success => {
+                    setSuccessBroad(success);
+                    console.log('iOS, Broadcasting Successful', success);
+                })
+                .catch(error => console.log('iOS, Broadcasting Error', error));
+            BLEAdvertiser.scan([12, 23, 56], {})
+                .then(success => {
+                    setSuccessScan(success);
+                    eventEmitter.addListener('onDeviceFound', (event) => {
+                        console.log(event) // "device data"
+                        setDevices([...devices, event]);
+                    });
+                    console.log('iOS, Scan Successful', success);
+                })
+                .catch(error => console.log('iOS, Scan Error', error));
+        });
 
         return () => {
             eventEmitter.removeAllListeners('onDeviceFound');
