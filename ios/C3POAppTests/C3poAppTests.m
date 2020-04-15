8663595dbae1509015d60fc7485b6c5d0d755dc9
Florian Guitton <florian@secretarium.com>
2020-04-15 09:12:52 +0100

    chore: Using proper case for C3PO iOS App

--- ios/C3POAppTests/C3poAppTests.m
+++ ios/C3POAppTests/C3poAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface C3poAppTests : XCTestCase
+@interface C3POAppTests : XCTestCase
 
 @end
 
-@implementation C3poAppTests
+@implementation C3POAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
